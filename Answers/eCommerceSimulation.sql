9773db52796c8b61ee5e6c5cccb45bd3d7f28d94
MegaBeedrillTAO <jakekremling@yahoo.com>
2019-09-12 08:30:50 -0500

    Finished

--- Answers/eCommerceSimulation.sql
+++ Answers/eCommerceSimulation.sql
@@ -27,3 +27,51 @@ values
 ('Wizz', 34.66),
 ('Bang', 23.09),
 ('Boom', 12.68);
+
+INSERT INTO orders
+(product_id)
+VALUES
+(1), (2), (3);
+
+SELECT * 
+FROM products 
+INNER JOIN orders 
+ON products.product_id = orders.product_id
+WHERE orders.order_id = 1;
+
+SELECT * FROM orders;
+
+SELECT orders.order_id, SUM(products.price)
+FROM products 
+INNER JOIN orders 
+ON products.product_id = orders.product_id
+WHERE orders.order_id = 3
+GROUP BY orders.order_id;
+
+ALTER TABLE users
+ADD COLUMN order_id INT
+REFERENCES orders(order_id);
+
+UPDATE users
+SET order_id = 1
+WHERE user_id = 1;
+
+UPDATE users
+SET order_id = 2
+WHERE user_id = 2;
+
+UPDATE users
+SET order_id = 3
+WHERE user_id = 3;
+
+SELECT *
+FROM users 
+INNER JOIN orders 
+ON orders.order_id = users.order_id
+WHERE users.user_id =3;
+
+SELECT COUNT(*)
+FROM users 
+INNER JOIN orders 
+ON orders.order_id = users.order_id
+WHERE users.user_id = 2;
